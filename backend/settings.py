04dee0407f332a6bd17b1e0af032285207f994dc
asad <ahmed.asad19@gmail.com>
2019-12-24 14:34:16 +0530

    TOken

--- backend/settings.py
+++ backend/settings.py
@@ -62,11 +62,11 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-    'api.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    # 'api.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
