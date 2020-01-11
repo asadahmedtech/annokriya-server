aebece98632fe7bf921aff496ec7fac0f6647131
asad <ahmed.asad19@gmail.com>
2020-01-11 22:18:06 +0530

    file upload

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
     'dashboard',
     'merger',
     'storages',
-    'knox'
+    'knox',
+    'fileupload'
 ]
 
 MIDDLEWARE = [
@@ -90,6 +91,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+##AMAZON AWS S3
+AWS_ACCESS_KEY_ID = 'AKIATM2X33VTPIJCHEBL'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'annokriya-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'backend/static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'backend.storage_backends.MediaStorage'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
