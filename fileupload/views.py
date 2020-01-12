cadade4f6511053ef7af0893885d7e632df1c104
asad <ahmed.asad19@gmail.com>
2020-01-12 13:43:02 +0530

    file upload

--- fileupload/views.py
+++ fileupload/views.py
@@ -10,8 +10,12 @@ def image_upload(request):
         image_file = request.FILES['image_file']
         image_type = request.POST['image_type']
         if settings.USE_S3:
+        	print("hi")
             upload = Upload(file=image_file)
+            print("hello")
+            print(upload.file)
             upload.save()
+            print("saved")
             image_url = upload.file.url
         else:
             fs = FileSystemStorage()
