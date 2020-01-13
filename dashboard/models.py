0dfca07826ff717cc1be7bbbaaf15934d844220f
asad <ahmed.asad19@gmail.com>
2020-01-14 01:17:28 +0530

    dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 # Create your models here.
 class Dashboard(models.Model):
-    user= models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    user= models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='Dashboard')
     pending = models.IntegerField(default=0)
     wrong = models.IntegerField(default=0)
     correct = models.IntegerField(default=0)
