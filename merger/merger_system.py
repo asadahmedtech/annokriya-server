77c1fb6e9d067a4ef0ea187eb8d35ada82e413ce
asad <ahmed.asad19@gmail.com>
2020-01-15 02:00:58 +0530

    merger

--- merger/merger_system.py
+++ merger/merger_system.py
@@ -282,8 +282,8 @@ class MergerSystemBoundingBox(object):
                     oy=oy/len(cluster[0][i])
                     ol=ol/len(cluster[0][i])
                     ow=ow/len(cluster[0][i])
-                    #out=BoundingBoxObject(x=str(ox),y=str(oy),l=str(ol),h=str(ow),taskid=entry.bb_taskgivenID)
-                    #out.save()
+                    out=BoundingBoxObject(x=str(ox),y=str(oy),l=str(ol),h=str(ow),taskid=entry.bb_taskgivenID)
+                    out.save()
                     # TaskPathBoundingBox.objects.filter(bb_taskgivenID=entry.bb_taskgivenID).delete()
                     ox=0
                     oy=0
@@ -299,22 +299,22 @@ class MergerSystemBoundingBox(object):
             key=list(user_count.keys())
             print(user_count)
             print(key)
-            # for i in range(0,MergerSystemBoundingBox.VALID-1,1):
-            #     if((user_count[key[i]]/maxi) >=self.CORRECT_THRESHOLD ):
-            #         d=Dashboard.objects.filter(user=key[i])
-            #         for db in d:
-            #             db.pending=db.pending-1
-            #             db.correct=db.correct+1
-            #             db.credits=str((db.credits) + 1 )
-            #             db.save()
-            #             break
-            #     else:
-            #         d=Dashboard.objects.filter(user=key[i])
-            #         for db in d:
-            #             db.pending=db.pending-1
-            #             db.wrong=db.wrong+1
-            #             db.save()
-            #             break
+            for i in range(0,len(key),1):
+                if((user_count[key[i]]/maxi) >=self.CORRECT_THRESHOLD ):
+                    d=Dashboard.objects.filter(user=key[i])
+                    for db in d:
+                        db.pending=db.pending-1
+                        db.correct=db.correct+1
+                        db.credits=str((db.credits) + 1 )
+                        db.save()
+                        break
+                else:
+                    d=Dashboard.objects.filter(user=key[i])
+                    for db in d:
+                        db.pending=db.pending-1
+                        db.wrong=db.wrong+1
+                        db.save()
+                        break
 
             # counts={}
             # for i in range(0,len(cluster[0]-2)):
