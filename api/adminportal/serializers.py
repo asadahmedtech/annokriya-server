702bd33b2afd960b2f1d10834d7a261114573a7f
asad <ahmed.asad19@gmail.com>
2020-01-16 20:50:10 +0530

    admin

--- api/adminportal/serializers.py
+++ api/adminportal/serializers.py
@@ -8,9 +8,9 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 1
+    max_page_size = 10
 
     def get_paginated_response(self, data):
         return Response({
