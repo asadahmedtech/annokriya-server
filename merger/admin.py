c38f998d927bafea9c058ee22cf3a81a20397120
asad <ahmed.asad19@gmail.com>
2020-01-19 20:50:32 +0530

    Changes

--- merger/admin.py
+++ merger/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import OutputTable, BoundingBoxObject, BoundingBoxObjectnew
+from .models import OutputTable, BoundingBoxObject, BoundingBoxObjectnew, BoundingBoxObjectall
 # Register your models here.
 admin.site.register(OutputTable)
 admin.site.register(BoundingBoxObject)
 admin.site.register(BoundingBoxObjectnew)
+admin.site.register(BoundingBoxObjectall)
